d6b8112d7e16d032f9f6bdb98996678f04e184aa
Renier Gaytan <renier.gaytan@gmail.com>
2024-10-24 16:35:19 -0600

    dummy commit

--- mongotask/src/db.js
+++
@@ -1,8 +0,0 @@
-const { MongoClient} = require ("mongodb");
-const DB_URL ="mongodb://localhost:27017/"
-const client = new MongoClient(DB_URL);
-const database = client.db ("Task");
-const Task = database.collection("task");
-module.exports = {
-    Task,
-};
